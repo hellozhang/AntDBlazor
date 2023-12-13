e59a7b5b3d8687e25586c7da8ce6f039d11ece05
hellozhang <hellozhangxf@qq.com>
2023-12-13 14:00:10 +0800

    2023年12月13日14:00:08

--- ChartService.cs
+++ ChartService.cs
@@ -1,109 +1,44 @@
-﻿using AntDesign;
-
-using EasyNotice.Dingtalk;
-
-using Flurl.Http;
-
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-
-using Org.BouncyCastle.Asn1.Crmf;
-
-
+﻿using Flurl.Http;
 using RestSharp;
-
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
-
-using static System.Net.WebRequestMethods;
-
 namespace ANTBlazor
 {
     public class ChartService : IChartService
     {
-        public async Task<string> GetSO()
+        HttpClient _http;
+        public ChartService(HttpClient http)
         {
-
-
-          //  // create request object
-          //  var request = new HttpRequestMessage(HttpMethod.Post, "https://jsonplaceholder.typicode.com/posts");
-
-          //  // set request body
-          //  var message2 = new
-          //  {
-          //      userId = 1,
-          //      id = 1,
-          //      title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-          //      body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-          //  };
-          //  request.Content = new StringContent(JsonSerializer.Serialize(message2), Encoding.UTF8, "application/json");
-
-          //  // add authorization header
-          //  //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "my-token");
-
-          //  // add custom http header
-          //  request.Headers.Add("My-Custom-Header", "foobar");
-
-          //  // send request
-          //  using var response = await HttpClient.SendAsync(request);
-
-          //  // convert response data to Article object
-          //var  article = await response.Content.ReadFromJsonAsync<Article>();
-
-
-
-
-
-            //string url = $"https://xizhi.qqoq.net/XZ375e1d6fb7f77722b95d07a80c4fe67a.send?title=\"白天\"&content=\"内容\"";
-
-            //var rs = await url.GetAsync();
-
-
-            //string url = $"https://xizhi.qqoq.net/XZ375e1d6fb7f77722b95d07a80c4fe67a.send?title=\"白天\"&content=\"内容\"";
-
-            //var rs = await url.PostAsync();
-
-
-
-            var message = new
+            _http = http;
+        }
+        public async Task<string> AddRecord()
+        {
+            var row = new
             {
-                msgtype = "markdown",
-                markdown = new
+                records = new[]
                 {
-                    title = "这个是设置的关键词",
-                    text = $"新订单：有新订单，请及时处理"
-                }
+                    new {
+                         fields= new
+                         {
+                              Name= "QWE33",
+                              Code="10902",
+                              StartDate="2023-11-02",
+                              StartValue= "1.09",
+                              CurrentValue=" 1.2"
+                          }
+                    }
+                },
+                fieldKey = "name"
             };
-
-            string webHook = "https://oapi.dingtalk.com/robot/send?access_token=dd9371044f8fa37f87d0e2d6aa7779e9e0fe5726fa37d512d07c153829a526ab";
-            string Secret = "SEC85ac2fe0afe69b9d7b74ab53e9858521247477bfa564668a98eac07ae4d5344e";
-            var requestUrl = DingTalkHelper.GetRequestUrl(webHook, Secret);
+            string requestUrl = "https://api.vika.cn/fusion/v1/datasheets/dst1CSXS5xqdZJHTLZ/records?viewId=viweVLj1Hwwqs&fieldKey=name";
             var res = await requestUrl
-              .WithHeader("Host", "oapi.dingtalk.com")
-                .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.160 Safari/537.36")
-              .WithHeader("Accept", "application/json")
-              .WithHeader("Access-Control-Allow-Origin", "*")
-                  .WithHeader("access_token", "dd9371044f8fa37f87d0e2d6aa7779e9e0fe5726fa37d512d07c153829a526ab")
-                      .WithHeader("timestamp", "1701846212301")
-                         .WithHeader("Origin", "https://oapi.dingtalk.com")
-                            .WithHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS")
-                             .WithHeader("Access-Control-Allow-Headers", "origin,Content-Length,Content-Type,Authorization")
-                         .WithHeader("Referer", "https://oapi.dingtalk.com")
-                           .WithHeader("sign", "1H%2fBN1tJBXBQ57mWJUQdNHQLxJFPkcWSZhfSkhno1Dc%3d")
-                         
-
-              .PostJsonAsync(message);
-
-
-
-
-
-
-            return "";
+                .WithHeader("Authorization", "Bearer uskKX37HkZuodf8VkY7CiQ1")
+                .WithHeader("Accept", "application/json")
+                .PostJsonAsync(row);
+            var s = res.GetStringAsync();
+            return res.ToString();
         }
     }
     public interface IChartService
     {
-        Task<string> GetSO();
+        Task<string> AddRecord();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,62 +1,52 @@
 using ANTBlazor;
 
-using Flurl.Http;
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-
-using System.Net.Http.Json;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://oapi.dingtalk.com/") });
+builder.Services.AddScoped<IChartService, ChartService>();
 
 
 
-builder.Services.AddScoped(x => {
-    //var apiUrl = new Uri(builder.Configuration["apiUrl"]);
+//builder.Services.AddHttpClient("WebAPI",
+//        client => client.BaseAddress = new Uri("https://www.example.com/base"))
+//    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-    //// use fake backend if "fakeBackend" is "true" in appsettings.json
-    //if (builder.Configuration["fakeBackend"] == "true")
-    //    return new HttpClient(new FakeBackendHandler()) { BaseAddress = apiUrl };
+//builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+//    .CreateClient("WebAPI"));
+//using Microsoft.AspNetCore.Components.WebAssembly.Authentication
 
-    return new HttpClient() { BaseAddress = new Uri("https://oapi.dingtalk.com/") };
-});
+//builder.HostEnvironment.BaseAddress
+//builder.Services.AddEasyNotice(config =>
+//{
+//    config.IntervalSeconds = 10;//同一标题的消息，10秒内只能发一条，避免短时间内大量发送重复消息
+//    config.UseDingTalk(option =>
+//    {
+//        option.WebHook = "https://oapi.dingtalk.com/robot/send?access_token=dd9371044f8fa37f87d0e2d6aa7779e9e0fe5726fa37d512d07c153829a526ab";//通知地址
+//        option.Secret = "SEC85ac2fe0afe69b9d7b74ab53e9858521247477bfa564668a98eac07ae4d5344e";//签名校验
+//    });
+//                                                          //});
 
-
-
-
-
-builder.Services.AddScoped<IChartService, ChartService>();//builder.HostEnvironment.BaseAddress
-                                                          //builder.Services.AddEasyNotice(config =>
-                                                          //{
-                                                          //    config.IntervalSeconds = 10;//同一标题的消息，10秒内只能发一条，避免短时间内大量发送重复消息
-                                                          //    config.UseDingTalk(option =>
-                                                          //    {
-                                                          //        option.WebHook = "https://oapi.dingtalk.com/robot/send?access_token=dd9371044f8fa37f87d0e2d6aa7779e9e0fe5726fa37d512d07c153829a526ab";//通知地址
-                                                          //        option.Secret = "SEC85ac2fe0afe69b9d7b74ab53e9858521247477bfa564668a98eac07ae4d5344e";//签名校验
-                                                          //    });
-                                                          //});
-
-var message2 = new
-{
-    userId = 1,
-    id = 1,
-    title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-};
-var requestUrl = "https://jsonplaceholder.typicode.com/posts";
-//var res = await requestUrl
-//  .WithHeader("Host", "jsonplaceholder.typicode.com")
-//    .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.160 Safari/537.36")
-//  .WithHeader("Accept", "application/json")
-//  .WithHeader("Access-Control-Allow-Origin", "*")
-//             .WithHeader("Origin", "https://jsonplaceholder.typicode.com")
-//           .WithHeader("Referer", "https://jsonplaceholder.typicode.com")
-//                .WithHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS")
-//                 .WithHeader("Access-Control-Allow-Headers", "origin,Content-Length,Content-Type,Authorization")
-//  .PostJsonAsync(message2);
+//var message2 = new
+//{
+//    userId = 1,
+//    id = 1,
+//    title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//    body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//};
+//var requestUrl = "https://jsonplaceholder.typicode.com/posts";
+////var res = await requestUrl
+////  .WithHeader("Host", "jsonplaceholder.typicode.com")
+////    .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.160 Safari/537.36")
+////  .WithHeader("Accept", "application/json")
+////  .WithHeader("Access-Control-Allow-Origin", "*")
+////             .WithHeader("Origin", "https://jsonplaceholder.typicode.com")
+////           .WithHeader("Referer", "https://jsonplaceholder.typicode.com")
+////                .WithHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS")
+////                 .WithHeader("Access-Control-Allow-Headers", "origin,Content-Length,Content-Type,Authorization")
+////  .PostJsonAsync(message2);
 
 
 
